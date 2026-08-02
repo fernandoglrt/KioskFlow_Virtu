978e79983ef3162022ed5347e38cf3c6dfaa4f9b
fernandoglrt <fernandoglrt@gmail.com>
2026-08-02 14:39:15 -0300

    Corrige queda do envio de email e adiciona botao de reenvio no admin
    O worker do Celery morreu com um erro fatal (mensagem corrompida na
    fila do Redis) em 15/07 e ficou parado ~18 dias sem ninguem perceber
    porque nao tinha politica de restart. Adiciona restart:always no
    worker e no redis para o worker se recuperar sozinho de falhas assim.
    
    Tambem adiciona um botao "Reenviar e-mail" na listagem do admin da
    PesquisaGravatai para reenviar manualmente o e-mail de uma pesquisa
    especifica.

--- survey/admin.py
+++ survey/admin.py
@@ -1,5 +1,11 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.forms.models import model_to_dict
+from django.shortcuts import redirect
+from django.urls import path, reverse
+from django.utils.html import format_html
+
 from .models import PesquisaGravatai
+from .tasks import enviar_email_task
 
 @admin.register(PesquisaGravatai)
 class PesquisaGravataiAdmin(admin.ModelAdmin):
@@ -11,7 +17,8 @@ class PesquisaGravataiAdmin(admin.ModelAdmin):
         'regiao_residencia',
         'voto_presidente',
         'voto_senador',
-        'avaliacao_zaffallon'
+        'avaliacao_zaffallon',
+        'reenviar_email_button',
     )
 
     # Filtros laterais para cruzamento de dados ágil
@@ -28,3 +35,27 @@ class PesquisaGravataiAdmin(admin.ModelAdmin):
 
     # Ordenação decrescente (mais recentes primeiro)
     ordering = ('-id',)
+
+    def reenviar_email_button(self, obj):
+        url = reverse('admin:survey_pesquisagravatai_reenviar_email', args=[obj.pk])
+        return format_html('<a class="button" href="{}">Reenviar e-mail</a>', url)
+    reenviar_email_button.short_description = 'E-mail'
+
+    def get_urls(self):
+        custom_urls = [
+            path(
+                '<int:pk>/reenviar-email/',
+                self.admin_site.admin_view(self.reenviar_email_view),
+                name='survey_pesquisagravatai_reenviar_email',
+            ),
+        ]
+        return custom_urls + super().get_urls()
+
+    def reenviar_email_view(self, request, pk):
+        pesquisa = self.get_object(request, pk)
+        if pesquisa is None:
+            messages.error(request, 'Pesquisa não encontrada.')
+        else:
+            enviar_email_task.delay(model_to_dict(pesquisa))
+            messages.success(request, f'E-mail da pesquisa "{pesquisa.nome}" reenviado para a fila.')
+        return redirect(reverse('admin:survey_pesquisagravatai_changelist'))
